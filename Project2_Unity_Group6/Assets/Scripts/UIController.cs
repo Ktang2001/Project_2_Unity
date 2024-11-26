3ebd601ec92b3aaf4e3598ccf02b4b874e86fa02
devansh-013 <devansh.sharma2002ds@gmail.com>
2024-11-26 18:03:02 -0500

    changes after presenting

--- Project2_Unity_Group6/Assets/Scripts/UIController.cs
+++ Project2_Unity_Group6/Assets/Scripts/UIController.cs
@@ -5,31 +5,29 @@ using UnityEngine.UI;
 public class UIController : MonoBehaviour
 {
     public Button startButton;
-    public Button optionsButton;
-    public Toggle noiseToggle;
-    public string optionsSceneName = "OptionsScene";  
+    public Button QuitButton;
+    public Toggle noiseToggle;  
     public string gameSceneName = "GameScene";  
 
     void Start()
     {
         // Assigning button click listeners
         startButton.onClick.AddListener(StartGame);
-        optionsButton.onClick.AddListener(LoadOptionsScene);
+        QuitButton.onClick.AddListener(QuitGame);
         noiseToggle.onValueChanged.AddListener(ToggleNoise);
 
         // Initialize toggle state
         noiseToggle.isOn = AudioListener.volume > 0;
     }
 
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
     void StartGame()
     {
         // Load the main game scene
         SceneManager.LoadScene("StarterScene");
-    }
-
-    void LoadOptionsScene()
-    {
-        SceneManager.LoadScene("OptionsScreen");
     }
 
     void LoadMenuScene()
